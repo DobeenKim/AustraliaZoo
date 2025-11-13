110054b42762f11a4bda18292e85ffb294b18b96
Liting <janetli82@gmail.com>
2025-11-13 16:52:12 +0100

    fix again

--- js/script.js
+++ js/script.js
@@ -1,55 +1,35 @@
- let mammal = document.querySelector(".first");
- let bird = document.querySelector(".second");
- let reptile = document.querySelector(".third");
-
-
-// let box2 = document.querySelector(".box2");
-// let box3 = document.querySelector(".box3")
-
-// box2.style.display = box2.style.display === "none" ? "block" : "none";
-//   box3.style.display = box3.style.display === "none" ? "block" : "none";
-
-function createSidebar() {
-    let sideLink = document.querySelector('.side-link');
-    const lis = [
-        { li: 'mammals', text: 'Echidna'},
-        { li: 'birds', text: 'Tasmanian Devil'},
-        { li: 'reptiles', text: 'quokka'},
-        
-    ];
-
-    lis.forEach(link => {
-        const newContent = document.createElement('li');
-        newContent.li = link.li;
-        newContent.textContent = link.text;
-        newContent.className = 'sidebar-link';
-        sideLink.appendChild(newContent);
-    });
+function Animal(kinds, name){
+    this.kinds = kinds,
+    this.name = name
 }
 
-function createSidebar2() {
-    let tas = document.querySelector('.tas');
-    const lis = [
-        { li: 'mammals', text: 'Cassowary'},
-        { li: 'birds', text: 'Kookaburra'},
-        { li: 'reptiles', text: 'Yellow Tailed Black Cockatoo'},
-        
-    ];
+let mammal = new Animal("Mammals", ["Echidna", "Tasmanian Devil", "Quokka"]);
+let bird = new Animal("Birds", ["Cassowary", "Kookaburra", "Yellow Tailed Black Cockatoo"]);
+let reptile = new Animal("Reptiles", ["frill-necked lizard", "Hawksbill Turtle", "Perentie"]);
+let animalArray = [mammal, bird, reptile];
 
-    lis.forEach(link => {
-        const newContent = document.createElement('li');
-        newContent.li = link.li;
-        newContent.textContent = link.text;
-        newContent.className = 'sidebar-link';
-        tas.appendChild(newContent);
+animalArray.forEach(animal => {
+    let animalList = document.querySelector(`.${animal.kinds}`);
+     let titleElement = document.querySelector(`.${animal.kinds.toLowerCase()}Title`);
+    animal.name.forEach(animalName => {
+        let animalContainer = document.createElement("li");
+        animalContainer.textContent = animalName;
+        animalContainer.style.display = "none";
+        animalList.appendChild(animalContainer);
+      if (titleElement) {
+        titleElement.addEventListener("click", () => {
+           
+            const animalItems = animalList.querySelectorAll("li");
+            const isHidden = animalItems[0].style.display === "none";
+            
+            animalItems.forEach(item => {
+                item.style.display = isHidden ? "block" : "none";
+            });
+        })
+      }
     });
-}
+       
+})
+        
+            
 
-
- mammal.addEventListener('click', createSidebar);  
- bird.addEventListener('click', createSidebar2);  
- reptile.addEventListener('click', createSidebar);  
- 
-        // { li: 'mammals', text: 'frill-necked lizard'},
-        // { li: 'birds', text: 'Hawksbill Turtle'},
-        // { li: 'reptiles', text: 'Perentie '}
