96010dd74cf8580c098e3ababfa19c71e90269cb
wineynia <gabiybiy@gmail.com>
2025-11-19 13:43:58 +0100

    Fixing toggle menu

--- js/script.js
+++ js/script.js
@@ -3,22 +3,5 @@ const toggleMenu = () => {
   menu.classList.toggle("show");
 };
 
-const closeMenuOnClickOutside = (event) => {
-  const menu = document.querySelector(".nav-menu");
-  const toggleButton = document.querySelector(".menu-toggle");
-
-  console.log("Clicked element:", event.target);
-
-  if (
-    !menu.contains(event.target) &&
-    !toggleButton.contains(event.target) &&
-    menu.classList.contains("show")
-  ) {
-    console.log("Closing menu");
-    menu.classList.remove("show");
-  }
-};
-
 document.querySelector(".menu-toggle").addEventListener("click", toggleMenu);
-document.addEventListener("click", closeMenuOnClickOutside);
 
