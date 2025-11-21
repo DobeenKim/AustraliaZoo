0d452234dbe4a20ab78c310af968bd4c7cbd2703
Maho Mikami <maho.cx1029@gmail.com>
2025-11-21 21:48:22 +0100

    add new font and fix some html tags

--- js/about.js
+++ js/about.js
@@ -1,77 +1,77 @@
-function Member(img,nickname,name,favAnimal,email,linkedin,github){
-    this.img = img,
-    this.nickname = nickname,
-    this.name = name,
-    this.favAnimal = favAnimal,
-    this.email = email,
-    this.linkedin =linkedin,
-    this.github =github
+function Member(img, nickname, name, favAnimal, email, linkedin, github) {
+  (this.img = img),
+    (this.nickname = nickname),
+    (this.name = name),
+    (this.favAnimal = favAnimal),
+    (this.email = email),
+    (this.linkedin = linkedin),
+    (this.github = github);
 }
 
 let dobeen = new Member(
-    "./images/raccoon.jpg",
-    "Dodo",
-    "Dobeen",
-    "Raccoon",
-    "Dobeen.Kim@edu.futuregame.se",
-    "https://www.linkedin.com/in/dobeen-kim",
-    "https://github.com/DobeenKim"
-)
+  "./images/raccoon.jpg",
+  "Dodo",
+  "Dobeen",
+  "Raccoon",
+  "Dobeen.Kim@edu.futuregame.se",
+  "https://www.linkedin.com/in/dobeen-kim",
+  "https://github.com/DobeenKim"
+);
 
 let claudia = new Member(
-    "./images/panda.jpg",
-    "Claudia",
-    "Claudia",
-    "Panda",
-    "Claudia.Pereira@edu.futuregames.se",
-    "https://www.linkedin.com/in/claudia-pereira-carion/",
-    "https://github.com/claudiacarion"
-)
+  "./images/panda.jpg",
+  "Claudia",
+  "Claudia",
+  "Panda",
+  "Claudia.Pereira@edu.futuregames.se",
+  "https://www.linkedin.com/in/claudia-pereira-carion/",
+  "https://github.com/claudiacarion"
+);
 
 let ting = new Member(
-    "./images/dragon.jpg",
-    "Tingting",
-    "Ting",
-    "Dog",
-    "Ting.Li@edu.futuregames.se",
-    "http://www.linkedin.com/in/ting-li-218540382",
-    "https://github.com/janetli82-ui"
-)
+  "./images/dragon.jpg",
+  "Tingting",
+  "Ting",
+  "Dog",
+  "Ting.Li@edu.futuregames.se",
+  "http://www.linkedin.com/in/ting-li-218540382",
+  "https://github.com/janetli82-ui"
+);
 
 let gabriella = new Member(
-    "./images/sloth01.jpg",
-    "Wineynia",
-    "Gabriella",
-    "Sloth",
-    "Gabriella.Arif@edu.futuregames.se",
-    "https://github.com/wineynia",
-    "https://www.linkedin.com/in/gabriella-a-99a903121b",
-)
+  "./images/sloth01.jpg",
+  "Wineynia",
+  "Gabriella",
+  "Sloth",
+  "Gabriella.Arif@edu.futuregames.se",
+  "https://github.com/wineynia",
+  "https://www.linkedin.com/in/gabriella-a-99a903121b"
+);
 
 let maho = new Member(
-    "./images/sloth02.jpg",
-    "MC Mahaa",
-    "Maho",
-    "Sloth",
-    "Maho.Kurauchi@edu.futuregames.se",
-    "http://www.linkedin.com/in/maho-kurauchi",
-    "https://github.com/mamahimiko"
-)
+  "./images/sloth02.jpg",
+  "MC Mahaa",
+  "Maho",
+  "Sloth",
+  "Maho.Kurauchi@edu.futuregames.se",
+  "http://www.linkedin.com/in/maho-kurauchi",
+  "https://github.com/mamahimiko"
+);
 
 let mainContent = document.querySelector(".main-content");
-let memberArray = [dobeen,claudia,ting,gabriella,maho]
+let memberArray = [dobeen, claudia, ting, gabriella, maho];
 
 mainContent.innerHTML = `
-<h2 class="message">Welcome to the About Page! <br>Meet our lovely staff!</h2>
+<h1 class="message">Welcome to the About Page! <br>Meet our lovely staff!</h2>
 `;
 
-memberArray.forEach(member => {
-    let preview = member.nickname;
-    let memberContainer = document.createElement("div")
-    const memberClassname = member.name.toLowerCase().replace(/\s+/g, "-")
-    memberContainer.classList.add("member-card",memberClassname)
+memberArray.forEach((member) => {
+  let preview = member.nickname;
+  let memberContainer = document.createElement("div");
+  const memberClassname = member.name.toLowerCase().replace(/\s+/g, "-");
+  memberContainer.classList.add("member-card", memberClassname);
 
-    memberContainer.innerHTML = `
+  memberContainer.innerHTML = `
     <h4 class="member-title">${member.name}</h4>
     <div class="image-wrapper">
         <img 
@@ -93,11 +93,7 @@ memberArray.forEach(member => {
     </div>
 `;
 
-    mainContent.appendChild(memberContainer);
+  mainContent.appendChild(memberContainer);
 
-    let memberDescription = memberContainer.querySelector(".member-description");
+  let memberDescription = memberContainer.querySelector(".member-description");
 });
-
-
-
-
--- js/mammals.js
+++ js/mammals.js
@@ -1,23 +1,33 @@
-function Animal(name, lifespan, group, food, description, length, weight, location, image) {
-  this.name = name,
-  this.lifespan = `${lifespan} years`,
-  this.group = group,
-  this.food = food,
-  this.description = description,
-  this.length = `${length} cm`,
-  this.weight = `${weight} kg`,
-  this.location = location;
+function Animal(
+  name,
+  lifespan,
+  group,
+  food,
+  description,
+  length,
+  weight,
+  location,
+  image
+) {
+  (this.name = name),
+    (this.lifespan = `${lifespan} years`),
+    (this.group = group),
+    (this.food = food),
+    (this.description = description),
+    (this.length = `${length} cm`),
+    (this.weight = `${weight} kg`),
+    (this.location = location);
   this.image = image;
-};
+}
 
 function Species(name, description) {
-  this.name = name,
-  this.description = description,
-  this.animals = [],
-  this.addAnimal = function (animal) {
-    this.animals.push(animal);
-  };
-};
+  (this.name = name),
+    (this.description = description),
+    (this.animals = []),
+    (this.addAnimal = function (animal) {
+      this.animals.push(animal);
+    });
+}
 
 const mammals = new Species(
   "Mammals",
@@ -66,11 +76,11 @@ mammals.addAnimal(
 let mainContent = document.querySelector(".main-content");
 
 mainContent.innerHTML = `
-  <h2 class="species-title">${mammals.name}</h2>
+  <h1 class="species-title">${mammals.name}</h2>
   <p class="species-info">${mammals.description}</p>
 `;
 
-mammals.animals.forEach(animal => {
+mammals.animals.forEach((animal) => {
   let preview = animal.description.substring(0, 200) + "...";
   let animalCard = document.createElement("div");
   const animalClassname = animal.name.toLowerCase().replace(/\s+/g, "-");
