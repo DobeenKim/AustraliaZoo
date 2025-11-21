430b94a4ec9eb0cda1e677f9f5fb7d71295380e0
Liting <janetli82@gmail.com>
2025-11-21 16:07:45 +0100

    fix

--- js/birds.js
+++ js/birds.js
@@ -23,7 +23,7 @@ function Birds(
 }
 
 let bird__one = new Birds(
-  "CassoWary",
+  "Cassowary",
   "It's not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water...",
   "Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida.",
   20,
